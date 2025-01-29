5c65fc34c7e010e9abb3b1e42dfc64850f8eb6cd
Samu Kangas <samu.kangas@metropolia.fi>
2025-01-29 09:39:16 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/~samukan/hybrid-react-build/',
+  // base: '/~samukan/hooks/',
   base: '',
   test: {
     environment: 'jsdom',
