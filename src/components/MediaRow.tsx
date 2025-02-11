9bbf2a2bee81820c78667390ed5e7bf2122a8e0d
Samu Kangas <samu.kangas@metropolia.fi>
2025-02-11 13:42:32 +0200

    Add edit and delete buttons for media items in MediaRow component

--- src/components/MediaRow.tsx
+++ src/components/MediaRow.tsx
@@ -1,5 +1,6 @@
 import {MediaItemWithOwner} from 'hybrid-types/DBTypes';
 import {Link} from 'react-router';
+import {useUserContext} from '../hooks/ContextHooks';
 
 type MediaItemProps = {
   item: MediaItemWithOwner;
@@ -7,12 +8,17 @@ type MediaItemProps = {
 };
 
 const MediaRow = (props: MediaItemProps) => {
-  const {item} = props;
+  const {item, setSelectedItem} = props;
+  const {user} = useUserContext();
+
+  const showEditDelete =
+    user && (user.level_name === 'Admin' || user.username === item.username);
+
   return (
     <tr className="bg-gray-800 transition-colors duration-200 hover:bg-gray-700">
       <td className="border-b border-gray-700 p-4">
         <img
-          className="rounded object-cover"
+          className="h-48 w-64 rounded object-cover"
           src={
             item.thumbnail ||
             (item.screenshots && item.screenshots[2]) ||
@@ -38,6 +44,22 @@ const MediaRow = (props: MediaItemProps) => {
           Show
         </Link>
       </td>
+      {showEditDelete && (
+        <td className="border-b border-gray-700 p-4">
+          <button
+            className="mr-2 rounded bg-stone-600 px-3 py-1 text-white transition-colors duration-200 hover:bg-stone-700"
+            onClick={() => console.log('modify', item)}
+          >
+            Modify
+          </button>
+          <button
+            className="rounded bg-stone-600 px-3 py-1 text-white transition-colors duration-200 hover:bg-stone-700"
+            onClick={() => console.log('delete', item)}
+          >
+            Delete
+          </button>
+        </td>
+      )}
     </tr>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: '/~samukan/hooks/',
+  // base: '/~samukan/tailwind/',
   base: '',
   test: {
     environment: 'jsdom',
